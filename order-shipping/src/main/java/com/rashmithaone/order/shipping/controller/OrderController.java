b42db032bc1d9c25af39a9c8fb044f59703feba3
Rashmi <147464476+RashmithaOne@users.noreply.github.com>
2023-10-24 20:50:21 -0500

    Update OrderController.java

--- order-shipping/src/main/java/com/rashmithaone/order/shipping/controller/OrderController.java
+++ order-shipping/src/main/java/com/rashmithaone/order/shipping/controller/OrderController.java
@@ -3,25 +3,48 @@ package com.rashmithaone.order.shipping.controller;
 import com.rashmithaone.order.shipping.dto.OrderShippingDTO;
 import com.rashmithaone.order.shipping.entity.Order;
 import com.rashmithaone.order.shipping.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
 
-    private final OrderService orderService;
-
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
+    @Autowired
+    private OrderService orderService;
 
     @PostMapping("/insert")
     public ResponseEntity<Order> saveOrder(@RequestBody OrderShippingDTO orderShippingDTO){
         return new ResponseEntity<>(orderService.saveOrder(orderShippingDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all-orders")
+    public ResponseEntity<List<OrderShippingDTO>> getAllOrders(){
+        return new ResponseEntity<>(orderService.getAllOrders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderShippingDTO> getOrder(@PathVariable int id){
+        return new ResponseEntity<>(orderService.getOrderDetails(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/update-orders")
+    public ResponseEntity<Order> getAllOrders(@RequestBody OrderShippingDTO orderShippingDTO){
+      return new ResponseEntity<>(orderService.updateOrder(orderShippingDTO), HttpStatus.OK);
+    }
+
+    @PutMapping("/update-orders-jpql")
+    @ResponseStatus(HttpStatus.OK)
+    public void getAllOrdersJpql(@RequestBody OrderShippingDTO orderShippingDTO){
+        orderService.updateOrderUsingCustomQuery(orderShippingDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrderById(@PathVariable int id){
+        orderService.deleteOrder(id);
     }
 }
