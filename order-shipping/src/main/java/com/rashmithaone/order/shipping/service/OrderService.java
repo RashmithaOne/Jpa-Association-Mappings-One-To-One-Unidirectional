ea6414e0d5f03bf0e096bdb55943ce11a538ebb8
Rashmi <147464476+RashmithaOne@users.noreply.github.com>
2023-10-24 20:49:25 -0500

    Update OrderService.java

--- order-shipping/src/main/java/com/rashmithaone/order/shipping/service/OrderService.java
+++ order-shipping/src/main/java/com/rashmithaone/order/shipping/service/OrderService.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -49,6 +52,62 @@ public class OrderService {
                 .orderValue(orderShippingDTO.getOrderValue())
                 .shipping(orderShippingDTO.getShipping())
                 .build();
+
+    }
+
+    public List<OrderShippingDTO> getAllOrders(){
+
+      return orderRepository.findAll()
+                .stream()
+              .map(this::entityToDto)
+              .collect(Collectors.toList());
+    }
+
+    public OrderShippingDTO getOrderDetails(int id){
+
+        Order order = orderRepository.findById(id).orElse(null);
+
+        OrderShippingDTO orderShippingDTO = entityToDto(order);
+        return orderShippingDTO;
+    }
+
+    public void updateOrderUsingCustomQuery(OrderShippingDTO orderShippingDTO){
+
+        if (orderShippingDTO != null){
+             Order order123 = orderRepository.findById(orderShippingDTO.getId()).orElse(null);
+             if (order123.getId() != null){
+                 Order order = dtoToEntity(orderShippingDTO);
+                 orderRepository.updateOrder(
+                         order.getId(),
+                         order.getTrackingNumber(),
+                         order.getDescription(),
+                         order.getDate(),
+                         order.getQuantity(),
+                         order.getOrderValue(),
+                         order.getShipping()
+                 );
+             }
+        }
+        else throw new RuntimeException("body cannot be null!...");
+    }
+
+    public Order updateOrder(OrderShippingDTO orderShippingDTO){
+
+        if (orderShippingDTO != null) {
+
+            Order order = dtoToEntity(orderShippingDTO);
+            Order savedOrder = orderRepository.save(order);
+
+            return savedOrder;
+        }
+            else throw new RuntimeException("body cannot be null!...");
+    }
+
+    public void deleteOrder(Integer id){
+        if(id !=null){
+            Order order = orderRepository.findById(id).orElse(null);
+            orderRepository.deleteById(order.getId());
+        }else throw new RuntimeException("id cannot be null");
 
     }
 }
